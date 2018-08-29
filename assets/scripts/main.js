0d625e3e5cebbe29155bda4b58de45ec773cee9a
Andrew James <ajames20@gmail.com>
2018-08-29 09:09:03 -0600

    Add input location

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,8 @@
 const navMenu = document.querySelector('.nav-bar');
+const accordionTabs = document.querySelector('.accordion-tabs');
+const radioButtons = document.getElementsByTagName('input');
 
-const menuToggle = event => {
+const menuToggle = () => {
   const navBarLinkSection = document.querySelector('.nav-bar-link-section');
   if (navBarLinkSection.style.display === 'none' && window.outerWidth < 1400 ) {
     navBarLinkSection.style.display = 'block';
@@ -9,4 +11,13 @@ const menuToggle = event => {
   }
 };
 
+const toggleImage = event => {
+  const plusSign = document.querySelectorAll('.tab-plus-sign');
+  console.log(plusSign.forEach(sign => console.log(sign.parentNode.previousSibling.previousSibling.checked)));
+  if (event.target.tagName === 'INPUT' && event.target.checked) {
+
+  }
+};
+
+accordionTabs.addEventListener('click', toggleImage);
 navMenu.addEventListener('click', menuToggle);
