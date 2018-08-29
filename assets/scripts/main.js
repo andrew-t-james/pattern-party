7bc8ae7152a3ca6c278baa846bb393848de0fcd3
Andrew James <ajames20@gmail.com>
2018-08-29 13:19:57 -0600

    Add tab toggle funcitonality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,4 @@
 const navMenu = document.querySelector('.nav-bar');
-const accordionTabs = document.querySelector('.accordion-tabs');
-const radioButtons = document.getElementsByTagName('input');
 
 const menuToggle = () => {
   const navBarLinkSection = document.querySelector('.nav-bar-link-section');
@@ -11,13 +9,4 @@ const menuToggle = () => {
   }
 };
 
-const toggleImage = event => {
-  const plusSign = document.querySelectorAll('.tab-plus-sign');
-  console.log(plusSign.forEach(sign => console.log(sign.parentNode.previousSibling.previousSibling.checked)));
-  if (event.target.tagName === 'INPUT' && event.target.checked) {
-
-  }
-};
-
-accordionTabs.addEventListener('click', toggleImage);
 navMenu.addEventListener('click', menuToggle);
