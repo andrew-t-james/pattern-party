966ccd12e67d95963f20b659804980d2cfbe8edc
Andrew James <ajames20@gmail.com>
2018-08-29 08:11:37 -0600

    Add toggle for mobile nav

---
+++ assets/scripts/main.js
@@ -0,0 +1,12 @@
+const navMenu = document.querySelector('.nav-bar');
+
+const menuToggle = event => {
+  const navBarLinkSection = document.querySelector('.nav-bar-link-section');
+  if (navBarLinkSection.style.display === 'none' && window.outerWidth < 1400 ) {
+    navBarLinkSection.style.display = 'block';
+  } else if (window.outerWidth < 1400) {
+    navBarLinkSection.style.display = 'none';
+  }
+};
+
+navMenu.addEventListener('click', menuToggle);
